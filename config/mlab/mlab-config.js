7db15c586f4d3a8c103575203ebf6982688f8d2f
Jake Overall <joverall22@gmail.com>
2017-11-14 12:04:00 -0700

    model example (BROKEN CODE)

---
+++ config/mlab/mlab-config.js
@@ -0,0 +1,18 @@
+// ESTABLISH WHERE THE DB LIVES
+var mongoose = require('mongoose')
+var connectionstring = 'mongodb://test:test@ds062448.mlab.com:62448/test-db'
+var connection = mongoose.connection;
+
+
+// Establishes MongoDb Connection
+mongoose.connect(connectionstring, {
+	useMongoClient: true,
+	keepAlive: { socketOptions: { keepAlive: 300000, connectTimeoutMS: 30000 } }
+});
+
+connection.on('error', console.error.bind(console, 'connection error:'))
+
+connection.once('open', function () {
+    console.log('We are connected to the db')
+});
+
